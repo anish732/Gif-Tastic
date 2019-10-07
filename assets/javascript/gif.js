5f52a49b9f3b0e6a7fc3ec24a835312e5392dbc0
Shital Patel <shital732@yahoo.com>
2019-10-06 23:56:48 -0400

    add bottons and imagdiv

---
+++ assets/javascript/gif.js
@@ -0,0 +1,98 @@
+var animal =["Cat","Dog","Rabbit","Monkey","Lion","Tiger","Bear"]
+
+function displayAnimalInfo(){
+    
+   var topics = $(this).attr("data-name")
+   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=1cBKXxYLRE9xRI7BWIUjTB2xHdAv7KT8&limit=10";
+
+   $.ajax({
+       url : queryURL,
+       method :"GET" 
+   }).then(function(response){
+    console.log(response);
+    console.log(queryURL)
+    //storing an array of result in result variable
+    var results = response.data;
+    //Looping over every result items
+    for (var i =0; i<results.length; i++){
+     //creating div for gif
+        var gifDiv = $("<div>");
+     //storing result items rating
+     var rating = results[i].rating;
+     //creating paragraph tag with result items rating
+     var p = $("<p>").text("Rating :" + rating);
+    //creating img tag
+     var animalImage = $("<img>")   
+     //Giving img tag src attribute from result
+     animalImage.attr("src", results[i].images.downsized_medium.url);
+     console.log(animalImage);
+     gifDiv.append(p);
+     gifDiv.append(animalImage);
+
+
+     $("#gif-appear-here").prepend(gifDiv);
+
+    }
+   })
+}
+
+//     function addAnimal(){
+//        var animalName = $(this).attr("data-name");
+//    }
+
+   function randerButtons(){
+       $("#button-view").empty()
+
+       for (var i=0; i<animal.length; i++){
+           var a = $("<button>");
+            a.addClass("animal");
+            a.attr("data-name",animal[i]);
+            a.attr("class","gif");
+            a.text(animal[i]);
+            $("#button-view").append(a);
+
+
+       }
+   }
+
+  // $("#button-view").on("click",function(){
+
+  // })
+// //    $(".gif").on('click',function(){
+// //        var x = $(this).data("name");
+// //        //console.log(x)
+// //        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=1cBKXxYLRE9xRI7BWIUjTB2xHdAv7KT8&limit=10";
+// //          console.log(queryURL);
+// //         $.ajax({
+// //             url : queryURL,
+// //            method : "GET",
+// //        }).then(function(response){
+// //            // $("#add-animal").text(JSON.stringify(response));
+// //         });
+
+
+// //    })
+//    var submit =function(){
+//     $("#add-animal").on("click",function(event){
+//        event.preventDefault();
+//        var y = $("#animal-input").val().trim();
+//        animal.push(y);
+//        console.log(animal)
+
+
+//        randerButtons();
+//    })
+
+// //     }
+// //    var displayGif = function(){
+// //       var btnVal = $(this).data("name");
+// //        console.log(btnVal);
+
+// }
+
+
+//    $(document).on("click",".gif",displayGif);
+
+    randerButtons();
+      //  submit();
+ 
